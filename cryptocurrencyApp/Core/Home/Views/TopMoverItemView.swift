796b1d520c5c5d76272b6834f2b728d973964d42
marvin evins <marvinevins@yahoo.com>
2022-09-24 16:41:18 -0500

    first commit

---
+++ cryptocurrencyApp/Core/Home/Views/TopMoverItemView.swift
@@ -0,0 +1,20 @@
+//
+//  TopMoverItemsView.swift
+//  cryptocurrencyApp
+//
+//  Created by marvin evins on 9/24/22.
+//
+
+import SwiftUI
+
+struct TopMoverItemsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TopMoverItemsView_Previews: PreviewProvider {
+    static var previews: some View {
+        TopMoverItemsView()
+    }
+}
---
+++ cryptocurrencyApp/Core/Home/Views/TopMoversView.swift
@@ -0,0 +1,20 @@
+//
+//  TopMoversView.swift
+//  cryptocurrencyApp
+//
+//  Created by marvin evins on 9/24/22.
+//
+
+import SwiftUI
+
+struct TopMoversView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TopMoversView_Previews: PreviewProvider {
+    static var previews: some View {
+        TopMoversView()
+    }
+}
