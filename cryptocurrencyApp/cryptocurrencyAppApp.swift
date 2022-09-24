b9e03eb183e4ea40b42ca50658d13977675077b3
marvin evins <marvinevins@yahoo.com>
2022-09-24 16:11:27 -0500

    Initial Commit

---
+++ cryptocurrencyApp/cryptocurrencyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  cryptocurrencyAppApp.swift
+//  cryptocurrencyApp
+//
+//  Created by marvin evins on 9/24/22.
+//
+
+import SwiftUI
+
+@main
+struct cryptocurrencyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
