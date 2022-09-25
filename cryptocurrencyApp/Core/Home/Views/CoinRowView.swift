2dbbc410bd88597ca06c643662bacd65c4c11e21
marvin evins <marvinevins@yahoo.com>
2022-09-24 21:12:29 -0500

    working on coin row view

--- cryptocurrencyApp/Core/Home/Views/CoinRowView.swift
+++ cryptocurrencyApp/Core/Home/Views/CoinRowView.swift
@@ -21,15 +21,40 @@ struct CoinRowView: View {
                 .resizable()
                 .scaledToFit()
                 .frame(width: 32, height: 32)
+                .foregroundColor(.orange)
             //coin name info
+            VStack(alignment: .leading, spacing: 4){
+                Text("Bitcoin")
+                    .font(.subheadline)
+                    .fontWeight(.semibold)
+                    .padding(.leading, 4)
+                
+                Text("BTC")
+                    .font(.caption)
+                    .padding(.leading, 6)
+            }
+            .padding(.leading, 2)
             
+            Spacer()
             
             //coin price info
-            
+            VStack(alignment: .trailing, spacing: 4){
+                Text("$20,330.00")
+                    .font(.subheadline)
+                    .fontWeight(.semibold)
+                    .padding(.leading, 4)
+                
+                Text("-5.60%")
+                    .font(.caption)
+                    .padding(.leading, 6)
+                    .foregroundColor(.red)
+            }
+            .padding(.leading, 2)
             
             
             
         }
+        .padding(.horizontal)
     }
 }
 
